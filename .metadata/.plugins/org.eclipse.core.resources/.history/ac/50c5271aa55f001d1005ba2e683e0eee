d5da022dc6eae1c04f6872a91b0682b223aeb2e3
Nicklas <nicklas.akerman@myy.haaga-helia.fi>
2022-11-10 12:25:56 +0200

    Etatehtava_6 kopio

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/ac/50c5271aa55f001d1005ba2e683e0eee
@@ -0,0 +1,72 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import model.Asiakas;
+import model.dao.Dao;
+
+@TestMethodOrder(OrderAnnotation.class)
+class JUnit_testaa_asiakkaat {
+
+	@Test
+	@Order(1) 
+	public void testPoistaKaikkiAsiakkaat() {
+		Dao dao = new Dao();
+		dao.removeAllItems("Nimda");
+		ArrayList<Asiakas> asiakkaat = dao.getAllItems();
+		assertEquals(0, asiakkaat.size());		
+	}
+	
+	@Test
+	@Order(2) 
+	public void testLisaaAsiakkaat() {
+		Dao dao = new Dao();
+		Asiakas asiakas_1 = new Asiakas("Pertti", "Perä", "0400123123", "pertti.perä@sähköposti.com");
+		Asiakas asiakas_2 = new Asiakas("aaaaaa", "bbbbb", "123123123", "etunimi@sukunimi.com");
+		Asiakas asiakas_3 = new Asiakas("mmmmmm", "nnnnnn", "333333333", "pertti.sukunimi@sposti.com");
+		Asiakas asiakas_4 = new Asiakas("masa", "valoton", "4032304032", "masa@valoton.com");
+		assertEquals(true, dao.addItem(asiakas_1)); //tai assertTrue(dao.addItem(Asiakas_1));	
+		assertEquals(true, dao.addItem(asiakas_2));
+		assertEquals(true, dao.addItem(asiakas_3));
+		assertEquals(true, dao.addItem(asiakas_4)); 	
+		assertEquals(4, dao.getAllItems().size());		
+	}
+	
+	
+	@Test
+	@Order(3) 
+	public void testMuutaAsiakas() {
+		//Muutetaan asiakkaan pertti = Pera
+		Dao dao = new Dao();		
+		ArrayList<Asiakas> asiakkaat = dao.getAllItems("Pertti");		
+		asiakkaat.get(0).setEtunimi("Pera");		
+		dao.changeItem(asiakkaat.get(0));
+		asiakkaat = dao.getAllItems("Pera");
+		assertEquals("A-1", asiakkaat.get(0).getEtunimi());
+		assertEquals("Perä", asiakkaat.get(0).getSukunimi());
+		assertEquals("0400123123", asiakkaat.get(0).getPuhelin());
+		assertEquals("pertti.perä@sähköposti.com", asiakkaat.get(0).getSposti());		
+	}
+	
+	@Test
+	@Order(4) 
+	public void testPoistaAsiakas() {
+		//Poistetaan se asiakas, jonka nimi on pera
+		Dao dao = new Dao();
+		ArrayList<Asiakas> asiakkaat = dao.getAllItems("Pera");
+		dao.removeItem(asiakkaat.get(0).getAsiakas_id());
+		assertEquals(0, dao.getAllItems("A-1").size());					
+	}
+	
+	@Test
+	@Order(5) 
+	public void testHaeOlematonAsiakas() {
+		//Haetaan asiakas,jonka id on -1
+		Dao dao = new Dao();
+		assertNull(dao.getItem(-1));
+	}
+}
---
+++ .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/Asiakkaat/scripts/main.js
@@ -0,0 +1,99 @@
+//funktio lomaketietojen muuttamiseksi JSON-stringiksi
+function serialize_form(form) {
+	return JSON.stringify(
+		Array.from(new FormData(form).entries())
+			.reduce((m, [key, value]) => Object.assign(m, { [key]: value }), {})
+	);
+}
+
+//funktio arvon lukemiseen urlista avaimen perusteella
+function requestURLParam(sParam) {
+	let sPageURL = window.location.search.substring(1);
+	let sURLVariables = sPageURL.split("&");
+	for (let i = 0; i < sURLVariables.length; i++) {
+		let sParameterName = sURLVariables[i].split("=");
+		if (sParameterName[0] == sParam) {
+			return sParameterName[1];
+		}
+	}
+}
+
+// Tutkitaan lisättävät tiedot ennen niiden lähettämistä backendiin
+function tutkiJaLisaa() {
+	if (tutkiTiedot()) {
+		lisaaTiedot();
+	}
+}
+
+// Tutkitaan lisättävät tiedot ennen niiden päivittämistä backendiin
+function tutkiJaPaivita() {
+	if (tutkiTiedot()) {
+		paivitaTiedot();
+	}
+}
+
+//funktio syöttötietojen tarkistamista varten (yksinkertainen)
+function tutkiTiedot() {
+	let ilmo = "";
+	if (document.getElementById("etunimi").value.length < 2) {
+		ilmo = "Etunimi ei kelpaa!";
+		document.getElementById("etunimi").focus();
+	} else if (document.getElementById("sukunimi").value.length < 2) {
+		ilmo = "Sukunimi ei kelpaa!";
+		document.getElementById("sukunimi").focus();
+	} else if (document.getElementById("puhelin").value.length < 7) {
+		ilmo = "puhelinnumero ei kelpaa!";
+		document.getElementById("puhelin").focus();
+	} else if (document.getElementById("sposti").value.length < 8 || document.getElementById("sposti").value.indexOf(".") == -1 || document.getElementById("sposti").value.indexOf("@") == -1) {
+		ilmo = "Sähköposti ei kelpaa!";
+		document.getElementById("sposti").focus();
+	}
+
+	if (ilmo != "") {
+		document.getElementById("ilmo").innerHTML = ilmo;
+		setTimeout(function() { document.getElementById("ilmo").innerHTML = ""; }, 3000);
+		return false;
+	} else {
+		document.getElementById("etunimi").value = siivoa(document.getElementById("etunimi").value);
+		document.getElementById("sukunimi").value = siivoa(document.getElementById("sukunimi").value);
+		document.getElementById("puhelin").value = siivoa(document.getElementById("puhelin").value);
+		document.getElementById("sposti").value = siivoa(document.getElementById("sposti").value);
+		return true;
+	}
+}
+
+//Funktio XSS-hyökkäysten estämiseksi (Cross-site scripting)
+function siivoa(teksti) {
+	teksti = teksti.replace(/</g, "");//&lt;
+	teksti = teksti.replace(/>/g, "");//&gt;
+	teksti = teksti.replace(/;/g, "");//&#59;	
+	teksti = teksti.replace(/'/g, "''");//&apos;
+	return teksti;
+}
+
+function varmistaPoisto(asiakas_id, nimi) {
+	if (confirm("Poista asiakas " + decodeURI(nimi) + "?")) { //decodeURI() muutetaan enkoodatut merkit takaisin normaaliksi kirjoitukseksi
+		poistaAsiakas(asiakas_id, nimi);
+	}
+}
+
+// Automaattisen fokuksen asettamiseen
+function asetaFocus(target) {
+	document.getElementById(target).focus();
+}
+
+//Funktio Enter-nappiin. Kutsu bodyn onkeydown()-metodista.
+function tutkiKey(event, target) {
+	if (event.keyCode == 13) {//13=Enter
+		//console.log(event.keyCode);
+		if (target == "listaa") {
+			haeAsiakkaat();
+		} else if (target == "lisaa") {
+			tutkiJaLisaa();
+		} else if (target == "paivita") {
+			tutkiJaPaivita();
+		}
+	} else if (event.keyCode == 113) {//F2
+		document.location = "listaaasiakkaat.jsp";
+	}
+}
