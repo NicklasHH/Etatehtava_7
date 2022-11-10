b01c7460d30d0b54031fb8247912164482cbdee0
Nicklas <nicklas.akerman@myy.haaga-helia.fi>
2022-11-10 14:55:55 +0200

    tehty tuotantoon muutokset

--- Asiakkaat/src/main/java/model/dao/Dao.java
+++ Asiakkaat/src/main/java/model/dao/Dao.java
@@ -1,6 +1,6 @@
 package model.dao;
 
-import java.io.File;
+//import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -20,10 +20,10 @@ public class Dao {
 	private Connection yhdista() {
 		Connection con = null;
 		String path = System.getProperty("catalina.base");
-		path = path.substring(0, path.indexOf(".metadata")).replace("\\", "/"); // Eclipsessa
+		//path = path.substring(0, path.indexOf(".metadata")).replace("\\", "/"); // Eclipsessa
 		//path =  new File(System.getProperty("user.dir")).getParentFile().toString() +"\\"; //Testauksessa
 		//System.out.println(path); //Tästä näet mihin kansioon laitat tietokanta-tiedostosi
-		//path += "/webapps/"; //Tuotannossa. Laita tietokanta webapps-kansioon
+		path += "/webapps/"; //Tuotannossa. Laita tietokanta webapps-kansioon
 		String url = "jdbc:sqlite:" + path + db;
 		try {
 			Class.forName("org.sqlite.JDBC");
